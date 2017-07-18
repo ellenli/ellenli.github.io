03439731ebf9fddb2d3c255cde5e6b205b1a5581
Ellen Li <ellen.li@shopify.com>
2017-07-17 23:37:24 -0400

    https

--- js/lastfm.js
+++ js/lastfm.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=enspirit&api_key=7fad6b09c99e8e11ccad0c568b4abf0d&format=json";
+  const url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=enspirit&api_key=7fad6b09c99e8e11ccad0c568b4abf0d&format=json";
   // feel free to use this js, but get your own api key at https://www.last.fm/api
 
   $.getJSON(url, function(data) {
